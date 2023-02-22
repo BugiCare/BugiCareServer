0542ef60a292f25a41c6d12282c27819fb95bd67
wonchanjoo <wcj0911@naver.com>
2023-02-22 15:46:41 +0900

    create new project

---
+++ src/main/java/hsu/bugicare/bugicareserver/BugiCareServerApplication.java
@@ -0,0 +1,13 @@
+package hsu.bugicare.bugicareserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugiCareServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BugiCareServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hsu/bugicare/bugicareserver/BugiCareServerApplicationTests.java
@@ -0,0 +1,13 @@
+package hsu.bugicare.bugicareserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugiCareServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
