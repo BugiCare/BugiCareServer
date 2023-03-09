085afde56ca60bc908ee4d54ab63eeaf2f595496
zzawang <hobin0116@naver.com>
2023-03-10 03:12:54 +0900

    Spring Boot + RDS (MySQL)

--- src/main/java/hsu/bugicare/bugicareserver/controller/UserController.java
+++ src/main/java/hsu/bugicare/bugicareserver/controller/UserController.java
@@ -18,7 +18,7 @@ public class UserController {
     public UserController(UserService userService) {
         this.userService = userService;
     }
-    
+
     // 테스트용
     @GetMapping("/userInfo")
     public User userInfo() {
