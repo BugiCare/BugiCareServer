ae37095ab70d69ecb1977e0dd589114b9fbe31f1
wonchanjoo <wcj0911@naver.com>
2023-04-06 19:21:53 +0900

    update service code for saving status of door and refrigerator

--- src/main/java/hsu/bugicare/bugicareserver/BugiCareServerApplication.java
+++ src/main/java/hsu/bugicare/bugicareserver/BugiCareServerApplication.java
@@ -1,6 +1,7 @@
 package hsu.bugicare.bugicareserver;
 
 import hsu.bugicare.bugicareserver.raspi.InfoFromRaspi;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,10 +10,16 @@ import java.io.IOException;
 @SpringBootApplication
 public class BugiCareServerApplication {
 
+    private final InfoFromRaspi server;
+
+    @Autowired
+    public BugiCareServerApplication(InfoFromRaspi server) throws IOException {
+        this.server = server;
+        server.start();
+    }
+
     public static void main(String[] args) throws IOException {
         SpringApplication.run(BugiCareServerApplication.class, args);
-        InfoFromRaspi server = new InfoFromRaspi();
-        server.start();
     }
 
 }
--- src/main/java/hsu/bugicare/bugicareserver/raspi/InfoFromRaspi.java
+++ src/main/java/hsu/bugicare/bugicareserver/raspi/InfoFromRaspi.java
@@ -1,6 +1,8 @@
 package hsu.bugicare.bugicareserver.raspi;
 
 import hsu.bugicare.bugicareserver.service.impl.FurnitureGraphService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -8,10 +10,15 @@ import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+@Component
 public class InfoFromRaspi {
 
     private final FurnitureGraphService furnitureGraphService;
 
+    private String oldRefrigeratorStatus = "closeRefrigerator";
+    private String oldDoorStatus = "closeDoor";
+
+    @Autowired
     public InfoFromRaspi(FurnitureGraphService furnitureGraphService) {
         this.furnitureGraphService = furnitureGraphService;
     }
@@ -29,12 +36,26 @@ public class InfoFromRaspi {
         BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
         String receivedText = null;
         while ((receivedText = in.readLine()) != null) {
+            // Refrigerator
             if(receivedText.contains("openRefrigerator")) {
-                furnitureGraphService.saveRefrigerator();
+                if(oldRefrigeratorStatus.equals("closeRefrigerator")) {
+                    furnitureGraphService.saveRefrigerator();
+                }
+                oldRefrigeratorStatus = "openRefrigerator";
+            } else if (receivedText.contains("closeRefrigerator")) {
+                oldRefrigeratorStatus = "closeRefrigerator";
             }
+
+            // Door
             if (receivedText.contains("openDoor")) {
-                furnitureGraphService.saveDoor();
+                if (oldRefrigeratorStatus.equals("closeDoor")) {
+                    furnitureGraphService.saveDoor();
+                }
+                oldRefrigeratorStatus = "openDoor";
+            } else if (receivedText.contains("closeDoor")) {
+                oldRefrigeratorStatus = "closeDoor";
             }
+
             System.out.println("socket으로부터 받은 메시지 : " + receivedText);
         }
         in.close();
