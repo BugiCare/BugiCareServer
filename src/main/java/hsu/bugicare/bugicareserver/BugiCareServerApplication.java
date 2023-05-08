2edf2fd536495fbe8c7766c1dd8fee971f185bcf
zzawang <hobin0116@naver.com>
2023-05-08 19:02:56 +0900

    delete Raspi Config

--- src/main/java/hsu/bugicare/bugicareserver/BugiCareServerApplication.java
+++ src/main/java/hsu/bugicare/bugicareserver/BugiCareServerApplication.java
@@ -1,7 +1,5 @@
 package hsu.bugicare.bugicareserver;
 
-import hsu.bugicare.bugicareserver.raspi.InfoFromRaspi;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,17 +7,8 @@ import java.io.IOException;
 
 @SpringBootApplication
 public class BugiCareServerApplication {
-
-    private static InfoFromRaspi server = null;
-
-    @Autowired
-    public BugiCareServerApplication(InfoFromRaspi server) throws IOException {
-        BugiCareServerApplication.server = server;
-    }
-
     public static void main(String[] args) throws IOException {
         SpringApplication.run(BugiCareServerApplication.class, args);
-        server.start();
     }
 
 }
--- src/main/java/hsu/bugicare/bugicareserver/service/impl/SocketServerService.java
+++
@@ -1,8 +0,0 @@
-package hsu.bugicare.bugicareserver.service.impl;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class SocketServerService {
-
-}
