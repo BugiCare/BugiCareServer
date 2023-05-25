8fcab00c681e58c1957433f1a0710b017de708bb
wonchanjoo <wcj0911@naver.com>
2023-05-25 12:43:47 +0900

    add logo, manager Image get API

--- src/main/java/hsu/bugicare/bugicareserver/controller/ManagerController.java
+++ src/main/java/hsu/bugicare/bugicareserver/controller/ManagerController.java
@@ -5,15 +5,24 @@ import hsu.bugicare.bugicareserver.dto.UserResponseDto;
 import hsu.bugicare.bugicareserver.service.ManagerService;
 import hsu.bugicare.bugicareserver.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 @RestController
 @CrossOrigin
 public class ManagerController {
+
     private final ManagerService managerService;
+    private static final String IMAGES_PATH = "../../src/main/resources/images";
+
     @Autowired
     public ManagerController(ManagerService managerService) {
         this.managerService = managerService;
@@ -34,4 +43,19 @@ public class ManagerController {
     public List<UserResponseDto> getMyUser(@RequestParam Long id) {
         return managerService.findMyUser(id);
     }
+
+    @GetMapping(value = "/logo", produces = MediaType.IMAGE_PNG_VALUE)
+    public ResponseEntity<Resource> getLogo() throws IOException {
+        Path imagePath = Paths.get(IMAGES_PATH, "bugicare_logo.png");
+        Resource resource = new UrlResource(imagePath.toUri());
+        return ResponseEntity.ok().contentType(MediaType.IMAGE_PNG).body(resource);
+    }
+
+    @GetMapping(value = "/manager_image", produces = MediaType.IMAGE_PNG_VALUE)
+    public ResponseEntity<Resource> getManagerImage() throws IOException {
+        Path imagePath = Paths.get(IMAGES_PATH, "manager_khs.png");
+        Resource resource = new UrlResource(imagePath.toUri());
+        return ResponseEntity.ok().contentType(MediaType.IMAGE_PNG).body(resource);
+    }
+
 }
