6132d813f5e49873f6e3f1261ed886abb0dff120
zzawang <hobin0116@naver.com>
2023-05-29 16:23:41 +0900

    remove log

--- src/main/java/hsu/bugicare/bugicareserver/BugiCareServerApplication.java
+++ src/main/java/hsu/bugicare/bugicareserver/BugiCareServerApplication.java
@@ -1,18 +1,13 @@
 package hsu.bugicare.bugicareserver;
 
-import hsu.bugicare.bugicareserver.config.LoggingInitializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 import java.io.IOException;
 
 @SpringBootApplication
 public class BugiCareServerApplication {
     public static void main(String[] args) throws IOException {
-        // 로깅 설정 초기화
-        LoggingInitializer.init();
-
         SpringApplication.run(BugiCareServerApplication.class, args);
     }
 
