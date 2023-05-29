859dfe680d43f19c4cec2cb33a08b1c6cabd01d4
zzawang <hobin0116@naver.com>
2023-05-29 15:52:39 +0900

    add log

--- src/main/java/hsu/bugicare/bugicareserver/BugiCareServerApplication.java
+++ src/main/java/hsu/bugicare/bugicareserver/BugiCareServerApplication.java
@@ -2,6 +2,7 @@ package hsu.bugicare.bugicareserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 import java.io.IOException;
 
