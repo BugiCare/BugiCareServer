a2746ca13e41e13967f003eb06ec7b6980f5df8a
EC2 Default User <ec2-user@ip-172-31-12-99.ap-northeast-2.compute.internal>
2023-06-01 10:09:06 +0000

    Revert "modify UserStatusGraphService"
    This reverts commit 2cda2ec9b044099937ce402650232b51be53eeb0.

--- src/main/java/hsu/bugicare/bugicareserver/repository/UserStatusRepository.java
+++ src/main/java/hsu/bugicare/bugicareserver/repository/UserStatusRepository.java
@@ -18,18 +18,26 @@ public interface UserStatusRepository extends JpaRepository<Sleep, Long> {
      */
 
     // DB에 저장되어 있는 값들 중 현재 시간과 비교하여 같은 일, 같은 시, 같은 분, 4초 전 ~ 같은 초 사이의 값 가져오기
+    @Query("SELECT w FROM Sleep w WHERE DATE(w.time) = CURRENT_DATE " +
+            "AND EXTRACT(HOUR FROM w.time) = EXTRACT(HOUR FROM CURRENT_TIMESTAMP)" +
+            "AND EXTRACT(MINUTE FROM w.time) = EXTRACT(MINUTE FROM CURRENT_TIMESTAMP)" +
+            "AND EXTRACT(SECOND FROM w.time) BETWEEN EXTRACT(SECOND FROM CURRENT_TIMESTAMP - 4) AND EXTRACT(SECOND FROM CURRENT_TIMESTAMP)")
+    List<Sleep> findDay();
+
+//    // DB에 저장되어 있는 값들 중 현재 시간과 비교하여 같은 일, 설정한 시, num 분의 레코드만 가져오기
 //    @Query("SELECT w FROM Sleep w WHERE DATE(w.time) = CURRENT_DATE " +
-//            "AND EXTRACT(HOUR FROM w.time) = EXTRACT(HOUR FROM CURRENT_TIMESTAMP)" +
-//            "AND EXTRACT(MINUTE FROM w.time) = EXTRACT(MINUTE FROM CURRENT_TIMESTAMP)" +
-//            "AND EXTRACT(SECOND FROM w.time) BETWEEN EXTRACT(SECOND FROM CURRENT_TIMESTAMP - 4) AND EXTRACT(SECOND FROM CURRENT_TIMESTAMP)")
-//    List<Sleep> findDay();
+//            "AND EXTRACT(HOUR FROM w.time) = EXTRACT(HOUR FROM CURRENT_TIMESTAMP) + :HMinus " +
+//            "AND EXTRACT(MINUTE FROM w.time) = EXTRACT(MINUTE FROM CURRENT_TIMESTAMP) + :mMinus " +
+//            "AND EXTRACT(SECOND FROM w.time) BETWEEN EXTRACT(SECOND FROM :beforeNum) AND EXTRACT(SECOND FROM :afterNum)")
+//    List<Sleep> findWeekOrMonth(@Param("beforeNum") int beforeNum, @Param("afterNum") int afterNum, @Param("mMinus") int mMinus, @Param("HMinus") int HMinus);
+
 
     // DB에 저장되어 있는 값들 중 현재 시간과 비교하여 같은 일, 같은 시, 같은 분, beforeNum ~ afterNum 초 사이의 값 가져오기
     @Query("SELECT w FROM Sleep w WHERE DATE(w.time) = CURRENT_DATE " +
             "AND EXTRACT(HOUR FROM w.time) = EXTRACT(HOUR FROM CURRENT_TIMESTAMP)" +
-            "AND EXTRACT(MINUTE FROM w.time) = EXTRACT(MINUTE FROM CURRENT_TIMESTAMP)" +
+            "AND EXTRACT(MINUTE FROM w.time) = EXTRACT(MINUTE FROM CURRENT_TIMESTAMP) + :minus " +
             "AND EXTRACT(SECOND FROM w.time) BETWEEN EXTRACT(SECOND FROM :beforeNum) AND EXTRACT(SECOND FROM :afterNum)")
-    List<Sleep> findDay(@Param("beforeNum") int beforeNum, @Param("afterNum") int afterNum);
+    List<Sleep> findDay(@Param("beforeNum") int beforeNum, @Param("afterNum") int afterNum, @Param("minus") int minus);
 
     // DB에 저장되어 있는 값들 중 현재 시간과 비교하여 같은 일, 설정한 시, num 분의 레코드만 가져오기
     @Query("SELECT w FROM Sleep w WHERE DATE(w.time) = CURRENT_DATE " +
